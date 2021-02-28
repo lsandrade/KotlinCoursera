a07908fbc70e174b143c622dfc2695c45277315f
Luan Andrade <luansandrade92@hotmail.com>
2021-02-28 15:01:48 -0300

    working with variables

--- src/intro/hello.kt
+++ src/intro/hello.kt
@@ -3,4 +3,17 @@ package intro
 fun main(args: Array<String>) {
     val name = if(args.isNotEmpty()) args[0] else "kotlin"
     println("Hello, $name!")
+
+    // variables. val = imutable. var = mutable
+    var number : Int = 0
+    number = 1
+    println("Number: $number")
+
+    // cant
+    val list = listOf("Java")
+    //list.add()
+
+    // can
+    val list2 = mutableListOf("Java")
+    list2.add("Kotlin")
 }
