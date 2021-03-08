560c5b0d7c7c00d519a166d65ad94b39bf92c0c3
Luan Andrade <luansandrade92@hotmail.com>
2021-03-07 23:14:31 -0300

    remove unecessary import

--- src/intro/hello.kt
+++ src/intro/hello.kt
@@ -1,7 +1,5 @@
 package intro
 
-import javax.print.attribute.IntegerSyntax
-
 fun main(args: Array<String>) {
     val name = if(args.isNotEmpty()) args[0] else "kotlin"
     println("Hello, $name!")
